9c1027e1a61aecee237e332f6d17956a075e1354
adrianaleitao9 <142885556+adrianaleitao9@users.noreply.github.com>
2024-05-27 16:00:44 +0100

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
+function updateWeather(response){
+    console.log(response.data);
+    let currentTemperature= document.querySelector(".city-weather-temperature");
+    let city= document.querySelector("#chosen-city");
+    city.innerHTML= `${capitalize(response.data.city)}`;
+    currentTemperature.innerHTML= Math.round(response.data.temperature.current);
+}
+
+function searchCity(city){
+let apiKey = "f41of6379tbae2fe2bd3f79075063741";
+let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`
+axios.get(apiUrl).then(updateWeather);
+}
+    
 function capitalize(text){
 return text.charAt(0).toUpperCase() + text.substring(1).toLowerCase();
 }
 
 function searchSubmit(event){
     event.preventDefault()
-    let searchInput=document.querySelector("#search-text-input")
-    let city=document.querySelector("#chosen-city");
-    city.innerHTML=`${capitalize(searchInput.value)}`;
+    let searchInput= document.querySelector("#search-text-input")
+    searchCity(searchInput.value);
 }
 
-let searchFormElement=document.querySelector("#search-form");
+let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",searchSubmit);
+
+searchCity("vila Real");
